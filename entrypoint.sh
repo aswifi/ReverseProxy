b12b35319981e00876e0107c4dd3dc1a28b4aee6
KCYLion <1095756998@qq.com>
2022-03-10 12:59:53 +0800

    init

---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [[ -z "${ProxySite}" ]]; then
+  s="s/proxy_pass/#proxy_pass/g"
+  echo "site:use local wwwroot html"
+else
+  s="s|\${ProxySite}|${ProxySite}|g"
+  echo "site: ${ProxySite}"
+fi
+
+rm -rf /etc/nginx/sites-enabled/default
+nginx -g 'daemon off;'
